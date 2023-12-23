ac92ae1dc1e38ccb1a80c4500182521fa42ff341
Yeonjun <wiv64@naver.com>
2023-12-24 00:50:01 +0900

    update setup_router_ar9271.py

--- setup_router_ar9271.py
+++ setup_router_ar9271.py
@@ -7,20 +7,18 @@ from pyudev import Context, Monitor, MonitorObserver
 
 # Global variable to keep track of the running process and device state
 lnxrouter_process = None
-device_connected = False
 
 def device_event(device):
-    global lnxrouter_process, device_connected
+    global lnxrouter_process
 
     if device.action == "add" and device.get('ID_VENDOR_ID') == '0cf3' and device.get('ID_MODEL_ID') == '9271':
         print("AR9271 chipset USB device connected. Activating WiFi AP.")
-        device_connected = True
         start_lnxrouter()
 
-    elif device.action == "remove" and device_connected:
-        print("AR9271 chipset USB device removed.")
-        device_connected = False
-        stop_lnxrouter()
+    elif device.action == "remove":
+        if not check_ar9271_connected():
+            print("AR9271 chipset USB device removed. Stopping WiFi AP.")
+            stop_lnxrouter()
 
 def start_lnxrouter():
     global lnxrouter_process
@@ -35,15 +33,19 @@ def stop_lnxrouter():
         lnxrouter_process.terminate()
         lnxrouter_process = None
 
+def check_ar9271_connected():
+    context = Context()
+    for device in context.list_devices(subsystem='usb'):
+        if device.get('ID_VENDOR_ID') == '0cf3' and device.get('ID_MODEL_ID') == '9271':
+            return True
+    return False
+
 context = Context()
 
 # Check for already connected devices at startup
-for device in context.list_devices(subsystem='usb'):
-    if device.get('ID_VENDOR_ID') == '0cf3' and device.get('ID_MODEL_ID') == '9271':
-        print("AR9271 chipset USB device already connected.")
-        device_connected = True
-        start_lnxrouter()
-        break
+if check_ar9271_connected():
+    print("AR9271 chipset USB device already connected.")
+    start_lnxrouter()
 
 monitor = Monitor.from_netlink(context)
 monitor.filter_by(subsystem='usb')
